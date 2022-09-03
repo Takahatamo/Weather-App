5768a4e820ac5c3f6d390dd0ea2d68b316870ea5
Ethan <esanjua3@my.centennialcollege.ca>
2022-09-03 00:57:37 -0400

    fixing

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import weather from "./modules/weather";
+import view from './modules/view';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const searchForm = document.getElementById("searchForm");
+const searchInput = document.getElementById("searchInput");
+const searchBtn = document.getElementById("searchBtn");
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+searchBtn.addEventListener("click", async () => {
+  if (searchInput.value === "") return;
+  const weatherData = await weather.getData(searchInput.value);
+  view.setSearchResult(weatherData);
+});
